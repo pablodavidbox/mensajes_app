85d5841da8654588f8fef75389d61e7b87681ec2
pablodavidbox <harlockraimar@gmail.com>
2022-03-12 21:24:55 -0300

    Todo el codigo

---
+++ src/main/java/com/mycompany/mensajes_app/Inicio.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.mensajes_app;
+
+import java.sql.Connection;
+
+/**
+ *
+ * @author Pablo David Schvager
+ */
+public class Inicio {
+    
+    public static void main(String[] args ){
+    
+        Conexion conexion = new Conexion();
+        
+        
+        try(Connection cnx = conexion.get_connection()){
+         
+        }catch (Exception e){    
+              System.out.println(e);
+        }
+        
+    }
+}
---
+++ src/main/java/com/mycompany/mensajes_app/Pepe.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.mensajes_app;
+
+/**
+ *
+ * @author Pablo David Schvager
+ */
+public class Pepe {
+    
+}
